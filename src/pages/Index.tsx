0e5e5c0946ea07b9e285bac90da1853d7c420031
Ajaz <ajazahammedck@gmail.com>
2025-08-08 17:43:57 +0530

    Updated Index.Tsx and App.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,7 @@
+// pages/Index.tsx
+import { LoginPage } from "@/components/LoginPage";
+
+export default function Index() {
+  // We don't need any props here because LoginPage handles its own navigation
+  return <LoginPage />;
+}

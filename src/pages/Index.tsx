4d6c0ef0bfc444c482599e42851173362ed74f37
Ajaz <ajazahammedck@gmail.com>
2025-08-09 05:52:54 +0530

    Loginpages

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,15 @@
+// // pages/Index.tsx
+// import { Loginpages } from "@/components/Loginpages";
+
+// export default function Index() {
+//   // We don't need any props here because LoginPage handles its own navigation
+//   return <Loginpages />;
+// }
+
+
 // pages/Index.tsx
-import { Loginpages } from "@/components/Loginpages";
+// Changed from named import { Loginpages } to default import Loginpages
+import Loginpages from "@/components/Loginpages";
 
 export default function Index() {
   // We don't need any props here because LoginPage handles its own navigation
